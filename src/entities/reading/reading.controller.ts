b08c29333b18f2a740ce4c1b5c65be3e3858cf70
lazaroofarrill <lazaroofarrill@gmail.com>
2021-10-26 20:16:49 -0400

    added csv endpoints

--- src/entities/reading/reading.controller.ts
+++ src/entities/reading/reading.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Logger } from '@nestjs/common'
+import { Controller, Get, Header, Logger, NotFoundException, Param, ParseIntPipe, Req } from '@nestjs/common'
 import { ReadingService } from './reading.service'
 import { CommonControllerController } from '../../common/common-controller/common-controller.controller'
 import { Reading } from './entities/reading.entity'
+import { parse } from 'json2csv'
 
 @Controller('reading')
 export class ReadingController extends CommonControllerController<ReadingService> {
   constructor(private readonly readingService: ReadingService) {
     super(readingService)
     this.logger = new Logger(Reading.name)
+  }
+
+  @Get('csv')
+  @Header('Content-Type', 'text/csv')
+  async getAllInCSV(@Req() req) {
+    const readings = await this.readingService.findAll(req)
+
+
+    const csv = parse(readings)
+    return csv
+  }
+
+  @Get('csv/:id')
+  @Header('Content-Type', 'text/csv')
+  async getOneCsv(@Param('id', ParseIntPipe) id: string, @Req() req) {
+    req.query.filter = {
+      $and: {
+        Station: {
+          id: id,
+        },
+      },
+    }
+    const readings = await this.readingService.findAll(req)
+
+    if (readings.length === 0) {
+      throw new NotFoundException()
+    }
+
+
+    const csv = parse(readings)
+    return csv
   }
 }
